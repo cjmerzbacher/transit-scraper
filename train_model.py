3e03fd80958c85fe48bda960545a95f60a94318a
Charlotte Merzbacher <cmerzbacher@natera.com>
2020-01-17 15:43:40 -0800

    Add basic travel function

--- train_model.py
+++ train_model.py
@@ -3,37 +3,71 @@ import scipy
 from matplotlib import pyplot
 from scipy.integrate import odeint
 
-train_types = ['b', 'l', 's', 'l']
+train_types = ['bullet', 'limited', 'local', 'limited']
 departures = [9, 10, 10.5, 10.75]
 
 class CalTrain:
     def __init__(self, train_types, departures):
         self.num_trains = len(train_types)
-        self.train_types = train_types
-        self.departures = departures
-        self.arrivals = self.calculate_arrival_times(train_types, departures)
-    def calculate_arrival_times(self, train_types, departures):
+        self.time = 0
+        self.trains = []
+        #Create trains in system
+        for n in range(len(train_types)):
+            self.trains.append(Train(train_types[n-1], departures[n-1]));
+
+    def calculate_arrival_times(self):
         arrivals = []
-        for i in range(len(train_types)):
-            if train_types[i] == 'b':
-                arrivals.append(departures[i]+10)
-            elif train_types[i] == 'l':
-                arrivals.append(departures[i]+15)
-            elif train_types[i] == 's':
-                arrivals.append(departures[i]+20)
-            else:
-                print('Not a valid train type!')
+        for t in self.trains:
+            arrivals.append(t.calculate_arrival_time())
         print(arrivals)
 
+    def get_train_positions(self):
+        positions = []
+        for t in self.trains: positions.append(t.position)
+        return positions
+
+    def get_train_speeds(self):
+        speeds = []
+        for t in self.trains: speeds.append(t.speed)
+        return speeds
+
+    def get_train_accelerations(self):
+        accels = []
+        for t in self.trains: accels.append(t.acceleration)
+        return accels
+
+    def get_limit_position(self, train):
+        position = train.position
+        positions = self.get_train_positions()
+        for i in range(len(positions)):
+            if positions[i] == position:
+                continue
+            elif positions[i] > position:
+                next_train_index = positions[i]
+                next_train = trains[i]
+        next_train = 10
+        next_stop = train.get_next_stop()
+        if next_stop < next_train:
+            print("Next limiting object is a stop", next_stop)
+            return next_stop
+        else:
+            print("Next limiting object is a train", next_train)
+            return next_train
+
+    def all_trains_travel(self):
+        for t in self.trains:
+            limit_position = self.get_limit_position(t)
+            t.travel(limit_position)
+
 class Train:
-    def __init__(self, train_type, arrival):
+    def __init__(self, train_type, departure):
         self.train_type = train_type
-        self.arrival = arrival
+        self.departure = departure
         self.position = 0
         self.speed = 0
-<<<<<<< HEAD
-        self.acceleration = 1
+        self.acceleration = 0
         self.speed_limit = 2
+        self.accel_limit = 1
         #Select the stop list based on train type
         if self.train_type ==  'bullet': self.stop_list = [0, 4, 8, 10]
         elif self.train_type ==  'limited': self.stop_list = [0, 2, 4, 5, 8, 10]
@@ -48,52 +82,32 @@ class Train:
                 return self.position
             elif self.position < stop_list[i]:
                 #print("Next stop", stop_list[i])
-=======
-
-        #Select the stop list based on train type
-        switch (self.train_type){
-        case 'bullet': self.stop_list = [0, 4, 8, 10]
-            break;
-        case 'limited': self.stop_list = [0, 2, 4, 5, 8, 10]
-            break;
-        case 'local': self.stop_list = [0, 1, 2, 4, 5, 6, 8, 10]
-            break;
-        }
-
-    def get_next_stop(self):
-        for i in range(len(stop_list)):
-            if self.position == stop_list[i]:
-                print("At stop", self.position)
-                return self.position
-            elif self.position < stop_list[i]:
-                print("Next stop", stop_list[i])
->>>>>>> 5c00a3303bf99a764c4d2015646786c4372b4955
                 return stop_list[i]
         return "Not a valid position"
 
-    def get_departure_time(self):
-<<<<<<< HEAD
+    def calculate_arrival_time(self):
         print("Train has departed")
 
     def travel(self, limit_position):
         time_step = 0.1
         stop_distance = limit_position - self.position
-        stop_limit = np.sqrt(2*self.acceleration*stop_distance)
+        stop_limit = np.sqrt(2*self.accel_limit*stop_distance)
         if (stop_limit > self.speed_limit) and (self.speed < self.speed_limit+self.acceleration * time_step):
             #accelerate
+            self.acceleration = self.accel_limit
             self.speed +=  self.acceleration * time_step
             self.position += self.speed * time_step
         elif stop_limit < self.speed_limit:
             #decelerate
-            self.speed -=  self.acceleration * time_step
+            self.acceleration = -self.accel_limit
+            self.speed +=  self.acceleration * time_step
             self.position += self.speed * time_step
         else:
             #maintain constant velocity
             self.position += self.speed * time_step
 
-
-
-t = Train('local', 10.25)
-=======
-        pass
->>>>>>> 5c00a3303bf99a764c4d2015646786c4372b4955
+c = CalTrain(train_types, departures)
+c.all_trains_travel()
+c.get_train_positions()
+c.get_train_speeds()
+c.get_train_accelerations()
