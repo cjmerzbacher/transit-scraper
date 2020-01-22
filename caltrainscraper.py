084707fe0f33169596ec55fa9e3c5140443a8783
Charlotte Merzbacher <cmerzbacher@natera.com>
2020-01-22 14:13:46 -0800

    Create timeframe for scraper to run over multiple minutes

--- caltrainscraper.py
+++ caltrainscraper.py
@@ -2,37 +2,49 @@ import requests
 import json
 from trip import Trip
 import codecs
+from datetime import datetime
+import time
 
 my_key = '7f7684c2-dc19-4627-a6b8-78c4b69d1c25'
 ct_position_url = "http://api.511.org/transit/vehiclepositions?api_key=7f7684c2-dc19-4627-a6b8-78c4b69d1c25&agency=CT&format=json"
 
-response = requests.get(ct_position_url, timeout=5)
+min_count = 0
+total_min=3
 
-decoded_json = response.text.encode().decode('utf-8-sig')
-json_parsed = json.loads(decoded)
-
-#Scrape data once per minute, using system clock to synchronize
-
-#For each timestamp:
-ts = json_parsed['Header']['Timestamp']
 id_list = []
 trips = []
 
-for train in json_parsed['Entities']:
-    id = train['Id']
-    type = train['Vehicle']['Trip']['RouteId']
-    longitude = train['Vehicle']['Position']['Longitude']
-    latitude = train['Vehicle']['Position']['Latitude']
+while (min_count < total_min):
+    response = requests.get(ct_position_url, timeout=5)
+    now = datetime.now()
+    print(now)
+    decoded_json = response.text.encode().decode('utf-8-sig')
+    json_parsed = json.loads(decoded_json)
 
-    if ~(id in id_list):
-        id_list.append(id)
-        trip = Trip(id, type, [[ts, longitude, latitude]])
-        trips.append(trip)
-    else:
-        for t in trips:
-            if t.id == id:
-                t.add_timepoint(ts, longitude, latitude)
-                break
+    train_timestamp = json_parsed['Header']['Timestamp']
+
+
+
+    for train in json_parsed['Entities']:
+        id = train['Id']
+        type = train['Vehicle']['Trip']['RouteId']
+        longitude = train['Vehicle']['Position']['Longitude']
+        latitude = train['Vehicle']['Position']['Latitude']
+        if id in id_list:
+            for t in trips:
+                if t.id == id:
+                    t.add_timepoint(train_timestamp, longitude, latitude)
+                    break
+        else:
+            id_list.append(id)
+            trip = Trip(id, type, [[train_timestamp, longitude, latitude]])
+            trips.append(trip)
+
+
+
+    min_count+=1
+    time.sleep(60)
 
 for t in trips:
-    print(t.id, t.timepoints)
+    print("Train: ", t.id)
+    print(t.timepoints)
